08b0222feadc26645dfc0b851f5c84b134b9e01e
Hamed Dvrm <hamed.dvrm@gmail.com>
2023-08-05 11:12:55 +0330

    Tied up folder a bit

--- bubble-pop-unity/Bubble Pop/Assets/Scripts/AnareshtanLevel/Anarestan 1 Scripts/ObstacleGenerator.cs
+++ bubble-pop-unity/Bubble Pop/Assets/Scripts/AnareshtanLevel/Anarestan 1 Scripts/ObstacleGenerator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class AnarGenerator : MonoBehaviour
+public class ObstacleGenerator : MonoBehaviour
 {
     [SerializeField] GameObject m_anar;
     [SerializeField] GameObject m_obstacle;
