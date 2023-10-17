788bc97c950fcd8bfbb646fd68f5d67cb6d81a2f
HamedDvrm <hamed.dvrm@gmail.com>
2023-10-17 21:26:46 +0330

    Fixed the bug with handle not triggering.

--- bubble-pop-unity/Bubble Pop/Assets/Scripts/DoshanbeSuri/LeverHandleController.cs
+++ bubble-pop-unity/Bubble Pop/Assets/Scripts/DoshanbeSuri/LeverHandleController.cs
@@ -144,13 +144,13 @@ public class LeverHandleController : MonoBehaviour
     {
         if (m_isDragging)
         {
-            Debug.Log("Dragging");
+            Debug.Log("Stay And Dragging");
             return;
         }
 
         if (m_isLeverActive == false || m_numOfTries <= 0)
         {
-
+            Debug.Log("Staying without any impact");
             return;
         }
 
