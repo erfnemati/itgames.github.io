dade1060b00d51d0a3aa1650c4e49e8107c5f9fe
HoseinChoobin <hoseinchoobin@gmail.com>
2023-10-25 12:03:06 +0330

    opening last edit for demo 251023

--- bubble-pop-unity/Bubble Pop/Assets/Scripts/openinggame.cs
+++ bubble-pop-unity/Bubble Pop/Assets/Scripts/openinggame.cs
@@ -9,21 +9,26 @@ public class openinggame : MonoBehaviour
     [SerializeField] GameObject roban2;
     [SerializeField] GameObject openingvfx;
     [SerializeField] GameObject btn;
-    //[SerializeField] GameObject dialougept2;
-    [SerializeField] GameObject blockedpanel;
-    [SerializeField] GameObject overlay;
-    [SerializeField] GameObject dialb;
+    [SerializeField] GameObject handguide;
+    //[SerializeField] GameObject overlay;
+    [SerializeField] GameObject dialougebox;
+    [SerializeField] GameObject firsttxt;
+    [SerializeField] GameObject secondtxt;
+    [SerializeField] GameObject realbublebtn;
+    [SerializeField] GameObject fakebublebtn;
+    //[SerializeField] GameObject dialb;
     [SerializeField] GameObject syaremanlogo;
     [SerializeField] AudioSource popbtnsound;
     [SerializeField] AudioSource levelsound;
     [SerializeField] AudioSource openingdone;
     [SerializeField] AudioSource showlogo;
     
+
     // Start is called before the first frame update
     void Start()
     {
-        overlay.SetActive(true);
-        blockedpanel.SetActive(true);
+        //overlay.SetActive(true);
+        //blockedpanel.SetActive(true);
        // overlay.SetActive(true);
        // dialougept1.SetActive(true);
     }
@@ -31,20 +36,33 @@ public class openinggame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!dialb.active)
-            blockedpanel.SetActive(false);
+        //if (!dialb.active)
+        //    blockedpanel.SetActive(false);
     }
     public void res()
     {
-      //  overlay.SetActive(false);
-      //  dialougept1.SetActive(false);
-       // SceneManager.LoadScene("");
-
+        //  overlay.SetActive(false);
+        //  dialougept1.SetActive(false);
+        // SceneManager.LoadScene("");
+        firsttxt.SetActive(false);
+        secondtxt.SetActive(true);
         // Time.timeScale = 1.0f;
 
     }
+
+    public void resu2()
+    {
+        secondtxt.SetActive(false);
+        dialougebox.SetActive(false);
+        handguide.SetActive(true);
+       // overlay.SetActive(false);
+        realbublebtn.SetActive(true);
+        fakebublebtn.SetActive(false);
+    }
+
     public void open()
     {
+        handguide.SetActive(false);
         popbtnsound.Play();
         openingdone.Play();
         btn.SetActive(false);
