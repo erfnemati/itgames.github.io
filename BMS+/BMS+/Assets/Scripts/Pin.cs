395812184a057f6227d46cbe75c36a9aed37ac08
Working Hamed <hamed.dvrm@gmail.com>
2023-12-02 16:10:24 +0330

    Changed pin code (it is not chosen item script).

--- BMS+/BMS+/Assets/Scripts/Pin.cs
+++ BMS+/BMS+/Assets/Scripts/Pin.cs
@@ -1,13 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RTLTMPro;
+using UnityEngine.UI;
 
 public class Pin : MonoBehaviour
 {
     [SerializeField] PinColor m_pinColor;
+    [SerializeField] int m_numOfUsages;
+
+    //UI code here:
+    [SerializeField] RTLTextMeshPro m_numOfUsagesText;
+    [SerializeField] GameObject m_buttonFrame;
+
+    public Pin(PinColor pinColor)
+    {
+        m_pinColor = pinColor;
+    }
 
     public PinColor GetPinColor()
     {
         return m_pinColor;
     }
+
+    public void IncrementUsages()
+    {
+        m_numOfUsages++;
+        UpdateUi();
+    }
+
+    public void DecrementUsages()
+    {
+        m_numOfUsages--;
+        if(m_numOfUsages < 0)
+        {
+            m_numOfUsages = 0;
+        }
+        UpdateUi();
+    }
+
+    //UI code here:
+    public void UpdateUi()
+    {
+        m_numOfUsagesText.text = m_numOfUsages + "";
+    }
+
+    public void ResetPinUi()
+    {
+        m_buttonFrame.gameObject.SetActive(false);
+    }
+
+    
 }
