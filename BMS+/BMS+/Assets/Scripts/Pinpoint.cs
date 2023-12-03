357bd67063779088584dad5b1a4adf8b50b9e98a
Working Hamed <hamed.dvrm@gmail.com>
2023-12-03 12:08:17 +0330

    Added clickPinPoint function,worked on some ui stuff for pinpoint

--- BMS+/BMS+/Assets/Scripts/Pinpoint.cs
+++ BMS+/BMS+/Assets/Scripts/Pinpoint.cs
@@ -1,22 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pinpoint : MonoBehaviour
 {
     [SerializeField] List<Hexagon> m_ownedHexagons;
+
+    private Button m_pinPoint;
     private Pin m_currentPin = null;
-    
+
+    //Ui stuff here,maybe recycle later:
+    private float m_initialPinPointWidth;
+    private float m_initialPinPointHeight;
+    private Color m_initialPinPointColor;
+    [SerializeField] float m_fullPinPointHeigth;
+    [SerializeField] float m_fullPinPointWidth;
+    [SerializeField] Sprite m_redObject;
+    [SerializeField] Sprite m_yellowObject;
+    [SerializeField] Sprite m_blueObject;
+
+    private void GetInitialPinPointDimentions()
+    {
+        m_initialPinPointHeight = GetComponent<RectTransform>().rect.height;
+        m_initialPinPointWidth = GetComponent<RectTransform>().rect.width;
+    }
+
+    private void GetInitialColor()
+    {
+        m_initialPinPointColor = GetComponent<Image>().color;
+    }
+
+    private void Start()
+    {
+        GetInitialColor();
+        GetInitialPinPointDimentions();
+        m_pinPoint = GetComponent<Button>();
+        m_pinPoint.onClick.AddListener(()=>GetComponent<Pinpoint>().ClickPinPoint());
+    }
+
 
     private void ChangePinpointSprite()
     {
-        //For ui purposes
+        if (m_currentPin == null)
+        {
+            RemoveSprite();
+        }
+        else
+        {
+            AddSprite();
+        }
     }
 
-    public void AddPin(Pin chosenPin)
+    private void AddSprite()
+    {
+
+    }
+
+    private void RemoveSprite()
+    {
+
+    }
+
+    public void ClickPinPoint()
+    {
+        if(Player._instance.GetPlayerPin() == null)
+        {
+            return;
+        }
+        if (m_currentPin == null)
+        {
+            AddPin(Player._instance.GetPlayerPin());
+        }
+        else if (m_currentPin != null)
+        {
+            RemovePin();
+        }
+    }
+
+    private void AddPin(Pin chosenPin)
     {
         if (m_currentPin != null)
         {
+            Debug.Log("This pinpoint is full");
             return;
         }
         UpdateHexagonColorsFromPinAddition(chosenPin);
@@ -24,7 +90,7 @@ public class Pinpoint : MonoBehaviour
         
     }
 
-    public void RemovePin()
+    private void RemovePin()
     {
         if (m_currentPin == null)
         {
@@ -41,6 +107,7 @@ public class Pinpoint : MonoBehaviour
         {
             tempHexagon.DeleteColor(FromPinColorToHexagonColor(m_currentPin));
         }
+        m_currentPin = null;
     }
 
     private void UpdateHexagonColorsFromPinAddition(Pin chosenPin)
