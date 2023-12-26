e6648d43c958554d55fd4546fe02c2e259ceea2a
Working Hamed <hamed.dvrm@gmail.com>
2023-12-26 17:20:57 +0330

    Added start screen script.

---
+++ BMS+/BMS+/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            return;
+        }
+        Debug.Log("Pressed");
+        LoadNextLevel();
+    }
+
+
+    private void LoadNextLevel()
+    {
+        int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextLevelIndex = (currentLevelIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextLevelIndex);
+    }
+}
