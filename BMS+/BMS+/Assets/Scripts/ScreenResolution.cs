4d0866cdcdd0a55e6c70d417154e8be55cecff96
Working Hamed <hamed.dvrm@gmail.com>
2024-01-02 17:30:38 +0330

    completed 0.75 version of the game.

--- BMS+/BMS+/Assets/Scripts/ScreenResolution.cs
+++ BMS+/BMS+/Assets/Scripts/ScreenResolution.cs
@@ -1,22 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RTLTMPro;
 
 public class ScreenResolution : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Screen.height <= Screen.width)
-        {
-            SetPortraitResolution();
-        }
+        SetPortraitResolution();
     }
 
     private void SetPortraitResolution()
@@ -25,6 +17,6 @@ public class ScreenResolution : MonoBehaviour
         float nativeAspectRatio = 1920f / 1080f;
         float screenHeight = Screen.height;
         float screenWidth = screenHeight / nativeAspectRatio;
-        Screen.SetResolution(1080, 1920, true);
+        Screen.SetResolution(2160, 3840, true);
     }
 }
